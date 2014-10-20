483ad57146128b81d8c60c8b5b841ed9bc417048
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-10-20 20:24:54 +0100

    updated to new Template.dynamic API

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Semantic UI Modals, packaged to play nice with Meteor',
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/richsilv/semantic-ui-modal.git"
 
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.1");
+  api.versionsFrom("METEOR@0.9.4");
   /* Use or imply other packages.
 
    * Example:
@@ -18,7 +18,7 @@ Package.on_use(function (api) {
     * Add files that should be used with this
     * package.
     */
-   api.use('templating', 'client');
+   api.use('templating@1.0.8', 'client');
    api.use("richsilv:semantic-ui-sass@1.0.0", 'client');
    api.add_files('semantic-ui-modal.html', 'client');
    api.add_files('semantic-ui-modal.js', 'client');
